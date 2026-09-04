a50cf0c73b3f6813fe754343e1075e45e5605802
Yosef Emyayu <94130968+J0-Y0@users.noreply.github.com>
2026-09-04 16:24:38 +0300

    Uncomment encoding and tokenization code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { get_encoding } from "tiktoken";
 
-//const encoding = get_encoding("cl100k_base");
-//const tokens = encoding.encode("Hello world!");
+const encoding = get_encoding("cl100k_base");
+const tokens = encoding.encode("Hello world!");
 console.log(tokens.length);
 // console.log(encoding.decode([83, 8251, 2488, 382, 2212, 0]));
